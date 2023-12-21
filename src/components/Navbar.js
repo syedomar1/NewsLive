c074e0d07dec7faefdc6e6e4653d4eb954973fcc
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2023-12-21 12:24:10 +0530

    final commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,11 +25,6 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="#">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
                 <Link className="nav-link" to="/business">
                   Business
                 </Link>
